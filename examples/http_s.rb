210cddf5ecfee163dccc4c6d0d5abf06c31a130d
Martin Wilhelmi <martin.wilhelmi@akra.de>
2011-09-07 20:08:50 +0200

    add config examples for http/s and socks4

---
+++ examples/http_s.rb
@@ -0,0 +1,14 @@
+proxy do |data|
+  # p data
+  if data =~ /^CONNECT (.+):([0-9]+)?(.*)\r\n/
+    remote = "#{$1}:#{$2.to_i||443}"
+    { :remote => remote,
+      :reply => "HTTP/1.0 200 Connection established\r\nProxy-agent: Netscape-Proxy/1.1\r\n\r\n",
+      :data => '' }
+  elsif data =~ /\r\nHost: (.+):?(.+)?\r\n/
+    remote = "#{$1}:#{$2||80}"
+    { :remote => remote }
+  else
+    { :noop => true }
+  end 
+end
